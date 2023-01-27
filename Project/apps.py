0ceaee5e55ff322893ac2217de20abe9540db7a4
Curtis Poon <poonj9@mcmaster.ca>
2023-01-27 03:03:01 -0500

    Initial commit

---
+++ Project/apps.py
@@ -0,0 +1,11 @@
+#  Copyright (c) 2023.
+#  Created by Curtis Poon for SEPT 4IT3.
+#
+#  I have not shared my code with anyone without the required consent and annotations being present in their documentation. I have also ensured that all functions are referenced from the location that I sourced them from.
+
+from django.apps import AppConfig
+
+
+class SemesterprojectConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Project'
